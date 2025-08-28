134a1b8c1afe4471dc22b5f2ccfb579f46c987bd
Hsu, Chia-Wei <162130385+edwinchu0711@users.noreply.github.com>
2025-08-28 15:58:51 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,23 @@
 const CACHE_NAME = 'stupikid-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/script.js',
-  '/1534128869.ico'
+  '/stupikid/',
+  '/stupikid/index.html',
+  '/stupikid/script.js',
+  '/stupikid/1534128869.ico'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
